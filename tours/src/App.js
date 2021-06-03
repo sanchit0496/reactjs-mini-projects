0cafc7cd4ff06f975cc4a7f7112a1d9cebcfc8b2
Sanchit <sanchit0496@gmail.com>
2021-06-03 15:13:26 +0530

    updating tours

--- tours/src/App.js
+++ tours/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+
+
 
 const App = () => {
+    const url = 'https://course-api.com/react-tours-project';
+
+    const [tours, setTours] = useState([]);
+
+    const fetchTours = async () => {
+        const response = await fetch(url);
+        const tourdata = await response.json();
+        setTours(tourdata);
+        console.log({tours});
+    }
+
+
     return (
         <div>
-            App
+           <h1>Hello</h1>
         </div>
     )
 }
