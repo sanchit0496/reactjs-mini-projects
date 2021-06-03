5b8160c3164712d0456bb1186c97aa494625ab59
Sanchit <sanchit0496@gmail.com>
2021-06-03 16:21:19 +0530

    uploading

--- fetch-api-1/src/App.js
+++ fetch-api-1/src/App.js
@@ -1,9 +1,50 @@
-const App = () => {
-    return (
-        <div>
-            App
-        </div>
-    )
+import React from "react";
+
+class App extends React.Component{
+    state = {
+        isLoading: true,
+        users: [],
+        error: null
+    };
+
+    fetchUsers(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => this.setState({
+           users:data ,
+           isLoading: false
+        })
+        ).catch(error => this.setState({error, isLoading: false}))
+    }
+
+    componentDidMount(){
+        this.fetchUsers();
+    }
+    
+    render() {
+        const { isLoading, users, error } = this.state;
+        return (
+          <React.Fragment>
+            <h1>Random User</h1>
+            {error ? <p>{error.message}</p> : null}
+            {!isLoading ? (
+              users.map(user => {
+                const { username, name, email } = user;
+                return (
+                  <div key={username}>
+                    <p>Name: {name}</p>
+                    <p>Email Address: {email}</p>
+                    <hr />
+                  </div>
+                );
+              })
+            ) : (
+              <h3>Loading...</h3>
+            )}
+          </React.Fragment>
+        );
+      }
+
 }
 
 export default App
