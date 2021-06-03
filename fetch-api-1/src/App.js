ca889dd916f5ecd88c1fde81f7305b60a100ac70
Sanchit <sanchit0496@gmail.com>
2021-06-03 16:52:58 +0530

    update

--- fetch-api-1/src/App.js
+++ fetch-api-1/src/App.js
@@ -1,49 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
 
 class App extends React.Component{
-    state = {
-        isLoading: true,
-        users: [],
-        error: null
-    };
+    constructor(props){
+        super(props);
+        this.state = {
+            data: [],
+            isLoading: true,
+            error: null
+        }
+    }
 
-    fetchUsers(){
-        fetch('https://jsonplaceholder.typicode.com/users')
+    fetchUsers = () => {
+        fetch(`https://jsonplaceholder.typicode.com/users`)
         .then(response => response.json())
-        .then(data => this.setState({
-           users:data ,
-           isLoading: false
-        })
-        ).catch(error => this.setState({error, isLoading: false}))
+        .then(data => {
+            this.setState({
+                users:data,
+                isLoading: false
+            })
+        }).catch(error => this.setState({error, isLoading:false}));
     }
 
     componentDidMount(){
         this.fetchUsers();
     }
+
+    render(){
+        const {isLoading, users, error} = this.state;
+        return(
+            <React.Fragment>
+                <h1>Random User</h1>
+                {
+                error ? <p>error.message</p> : null
+                }
+                {
+                    !isLoading? (
+                        users.map(user => {
+                            const {username, name, email} = user;
+                            return(
+                                <div key = {username}>
+                                <p>Name: {name}</p>
+                                <p>Email: {email}</p>    
+                                <hr />
+                                </div>
+                            )
+                        })
+                    ) : (
+                        <h2>Loading</h2>
+                    )
+                }
+            </React.Fragment>
+        )
+    }
     
-    render() {
-        const { isLoading, users, error } = this.state;
-        return (
-          <React.Fragment>
-            <h1>Random User</h1>
-            {error ? <p>{error.message}</p> : null}
-            {!isLoading ? (
-              users.map(user => {
-                const { username, name, email } = user;
-                return (
-                  <div key={username}>
-                    <p>Name: {name}</p>
-                    <p>Email Address: {email}</p>
-                    <hr />
-                  </div>
-                );
-              })
-            ) : (
-              <h3>Loading...</h3>
-            )}
-          </React.Fragment>
-        );
-      }
 
 }
 
