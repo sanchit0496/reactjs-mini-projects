f5b49d024481260c8c941d2ece06e55a1ff98804
Sanchit <sanchit0496@gmail.com>
2021-06-03 11:35:31 +0530

    update

--- digital-clock/src/App.js
+++ digital-clock/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            clock : new Date()
+        }
+    }
+
+    ticking = () => {
+        setInterval(() => {
+            this.setState(new Date())
+        }, 1000)
+    }
+
+    render(){
+        return(
+            <h1>{this.state.clock.toLocaleString()}</h1>
+        )
+    }
 }
 
 export default App;
