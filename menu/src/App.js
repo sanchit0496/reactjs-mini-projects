c5fb3b429a9a412a52d0443c7ffaa264c7e25e46
Sanchit <sanchit0496@gmail.com>
2021-06-06 16:03:37 +0530

    adding the menu items

--- menu/src/App.js
+++ menu/src/App.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+import './index.css';
 
 const App = () => {
-    return (
-        <div>
-            App
-        </div>
-    )
+
+    const [menuItems, setMenuItems] = useState(items);
+    const [categories, setCategories] = useState([]);
+    
+
+    return(
+        <main>
+            <section className="menu section">
+                <div className="title">
+                    <h2>The Menu</h2>
+                    <div className="underline"></div>
+                </div>
+                <Categories />
+                <Menu items = {menuItems} />
+            </section>
+        </main>
+    );
 }
 
 export default App
---
+++ menu/src/Categories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Categories = () => {
+    return (
+        <div>
+            Categories
+        </div>
+    )
+}
+
+export default Categories
