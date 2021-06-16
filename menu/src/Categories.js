07d3315199eb9a8150a73581151d7310057e5cc5
Sanchit <sanchit0496@gmail.com>
2021-06-16 10:41:19 +0530

    adding menu

--- menu/src/Categories.js
+++ menu/src/Categories.js
@@ -1,18 +1,22 @@
 import React from 'react'
 
-const Categories = ({filterItems}) => {
+const Categories = ({categories, filterItems}) => {
     return (
         <div className="btn-container">
-
-            <button className="filter-btn" 
-            onClick = {() => filterItems('all')}>
-                All
-            </button>
-
-            <button className="filter-btn" 
-            onClick = {() => filterItems('breakfast')}>
-                breakfast
-            </button>
+            {
+                categories.map((category, index) => {
+                    return(
+                        <button
+                            type = 'button'
+                            className = 'filter-btn'
+                            key = {index}
+                            onClick = {() => filterItems(category)}
+                        >
+                            {category}
+                        </button>
+                    )
+                })
+            }
         </div>
     )
 }
