af8efe6819d3ebfffd5cdc1b07f4e190626f65d0
Sanchit <sanchit0496@gmail.com>
2021-06-24 02:11:54 +0530

    adding lorem ipsum process

--- lorem-ipsum/src/App.js
+++ lorem-ipsum/src/App.js
@@ -1,8 +1,39 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import data from './data';
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
 
-const App = () => {
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
+  return (
+    <section className='section-center'>
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>paragraphs:</label>
+        <input
+          type='number'
+          name='amount'
+          id='amount'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className='btn'>generate</button>
+      </form>
+      <article className='lorem-text'>
+        {text} 
+      </article>
+    </section>
+  );
 }
 
-export default App
+export default App;
