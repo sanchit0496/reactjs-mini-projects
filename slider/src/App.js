7f796563a0449ef2146139759194eac7fa9ca103
Sanchit <sanchit0496@gmail.com>
2021-06-24 01:00:48 +0530

    useeffect

--- slider/src/App.js
+++ slider/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
 
-const App = () => {
-    return (
-        <div>
-            App
-        </div>
-    )
+function App(){
+    const [people, setPeople] = useState(data)
+    const [index, setIndex] = React.useState(0)
+
+    useEffect(() => {
+        const lastIndex = people.length -1;
+        if(index < 0){
+            setIndex(lastIndex);
+        }
+        if(index > lastIndex){
+            setIndex(0);
+        }
+    }, [index, people])
+
+
+    
+
 }
 
 export default App
