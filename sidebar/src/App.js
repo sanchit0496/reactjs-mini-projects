33995835510c03af718bd425ac37227e57b7c3f9
Sanchit <sanchit0496@gmail.com>
2021-06-24 22:36:45 +0530

    updating

--- sidebar/src/App.js
+++ sidebar/src/App.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Sidebar from 'react-sidebar'
 
 const App = () => {
+
+    const [sidebarOpen, setSidebarOpen] = useState(false)
+
     return (
-        <div>
-            App
+
+
+    <div>
+                
+            <Sidebar
+                sidebar = { <h1>Hello</h1> }
+                open = {sidebarOpen}
+                onSetOpen = {sidebarOpen}
+            >
+                <button
+                onClick = {() => {
+                    if(sidebarOpen === false){
+                        setSidebarOpen(true)
+                    }
+                }}
+            >
+                Open
+            </button>
+            </Sidebar>
         </div>
     )
 }
