2f78c568e38349913a459aed9e22faee04796245
Sanchit <sanchit0496@gmail.com>
2021-06-24 11:18:34 +0530

    adding css

--- dark-mode/src/App.js
+++ dark-mode/src/App.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const App = () => {
+
+    const [darkmode, setDarkMode] = useState(false)
+    const body = document.getElementById('body');
+    const toggle = document.getElementById('toggle')
+
+    useEffect(() => {
+        if(darkmode === false){
+            body.classList.add('dark')
+        }else{
+            body.classList.add('light')
+        }
+    }, [darkmode])
+    
     return (
         <div>
-            App
+            <button
+            id = 'toggle'
+            onClick = {() => {alert('hello')}} 
+            >
+                Click Here To Toggle
+            </button>
         </div>
     )
 }
