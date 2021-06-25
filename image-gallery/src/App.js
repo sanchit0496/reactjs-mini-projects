2cfbf05160c80ea483b394f112f3c815e9e5ac9f
Sanchit <sanchit0496@gmail.com>
2021-06-25 22:38:46 +0530

    update

--- image-gallery/src/App.js
+++ image-gallery/src/App.js
@@ -4,20 +4,31 @@ export class App extends Component {
     constructor(props){
         super(props)
         this.state = {
-            pics: []
+            pics: [],
+            isLoading: false
         }
+        this.loadPics = this.loadPics.bind(this);
+        
+    }
+
+    componentDidMount(){
+        this.loadPics();
     }
 
     loadPics(){
-        let url =  'https://picsum.photos/v2/list';
+        let url =  `https://picsum.photos/v2/list`;
+
+        fetch(url)
+        .then(response => response.json())
+        .then(res => this.setState({pics: res, isLoading: true}))
+        
     }
-
-
 
     render() {
         return (
             <div>
                 App
+                
             </div>
         )
     }
