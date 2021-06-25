a843cd2a6291fa30f7022258dc35d439b5323d04
Sanchit <sanchit0496@gmail.com>
2021-06-25 20:47:08 +0530

    adding url

--- image-gallery/src/App.js
+++ image-gallery/src/App.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
 
 export class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            pics: []
+        }
+    }
+
+    loadPics(){
+        let url =  'https://picsum.photos/v2/list';
+    }
+
+
+
     render() {
         return (
             <div>
