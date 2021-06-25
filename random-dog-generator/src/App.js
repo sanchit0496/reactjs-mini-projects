44b8d18f55523741b1e8de3f7e762fa9bae3f5f3
Sanchit <sanchit0496@gmail.com>
2021-06-25 16:55:57 +0530

    v1

--- random-dog-generator/src/App.js
+++ random-dog-generator/src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
 
 export class App extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            imgSrc : ''
+        }
+        
+    }
+
+    componentDidMount(){
+        this.fetchImg();
+    }
+
+    fetchImg(){
+        let url = `https://api.giphy.com/v1/gifs/random?tag=dog&api_key=25epsat6s97tTNp44xAfHe91wwUHPh5W`;
+
+        fetch(url)
+        .then(response => response.json())
+        .then(res => this.setState({imgSrc: res.data.image_url}))
+        
+    }
+
     render() {
         return (
             <div>
-                App
+                <img src= {this.state.imgSrc} alt="" />
             </div>
         )
     }
