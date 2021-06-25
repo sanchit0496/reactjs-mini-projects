ac13bff2132752f8dc38601d6d11227cfe99de53
Sanchit <sanchit0496@gmail.com>
2021-06-25 16:44:21 +0530

    adding the class based

--- random-dog-generator/src/App.js
+++ random-dog-generator/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const App = () => {
-    return (
-        <div>
-            App
-        </div>
-    )
+export class App extends Component {
+    render() {
+        return (
+            <div>
+                App
+            </div>
+        )
+    }
 }
 
 export default App
