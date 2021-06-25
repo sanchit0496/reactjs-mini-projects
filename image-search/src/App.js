b28396c8f6a5cdb5e4342647b7caeccc626943e9
Sanchit <sanchit0496@gmail.com>
2021-06-26 03:09:16 +0530

    uploading

--- image-search/src/App.js
+++ image-search/src/App.js
@@ -5,24 +5,21 @@ class App extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            value: '',
             pics: []
         }
         this.fetchPics = this.fetchPics.bind(this)
     }
 
    
-    fetchPics(event){
-        event.preventDefault();
+    fetchPics(e){
+        e.preventDefault();
         let a = document.getElementById('term').value;
-        console.log(a);
+      
         let url = `https://pixabay.com/api/?key=22238304-8a144d939bdfe845e8130ae3b&q=${a}&image_type=photo`;
 
         fetch(url)
         .then(response => response.json())
-        .then(res => this.setState({pics: res.data}))
-        console.log(this.state.pics);
-
+        .then(res => this.setState({pics: res}))
     }
 
     render(){
@@ -32,6 +29,12 @@ class App extends React.Component{
                     <input type="text" id='term' placeholder = 'Enter Value'/>
                     <input type="submit"/>
                 </form>
+                
+                {
+                    this.state.pics.map((pic) => {
+                        return <img src= {pic.hits.largeImageURL} key={pic.hits.id} />
+                    })
+                }
             </div>
         )
     }
