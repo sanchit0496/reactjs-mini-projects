3701e51c523bf359d95a9b734cf6643929241d22
Sanchit <sanchit0496@gmail.com>
2021-06-26 21:02:25 +0530

    adding the idletimer widget

---
+++ idle-timer/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState, useRef } from 'react'
+import IdleTimer from 'react-idle-timer'
+import Modal from 'react-modal'
+
+Modal.setAppElement('#root')
+
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(true)
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const idleTimerRef = useRef(null)
+  const sessionTimeoutRef = useRef(null)
+  const onIdle = () => {
+    console.log('User is idle')
+    setModalIsOpen(true)
+    sessionTimeoutRef.current = setTimeout(logOut, 5000)
+  }
+
+  const logOut = () => {
+    setModalIsOpen(false)
+    setIsLoggedIn(false)
+    clearTimeout(sessionTimeoutRef.current)
+    console.log('User has been logged out')
+  }
+  const stayActive = () => {
+    setModalIsOpen(false)
+    clearTimeout(sessionTimeoutRef.current)
+    console.log('User is active')
+  }
+  return (
+    <div>
+      {isLoggedIn ? <h2>Hello There</h2> : <h2>Hello Guest</h2>}
+      <IdleTimer
+        ref={idleTimerRef}
+        timeout={1000 * 5}
+        onIdle={onIdle}
+      />
+      <Modal isOpen={modalIsOpen}>
+        <h2>You've been idle for a while!</h2>
+        <p>You will be logged out soon</p>
+        <div>
+          <button onClick={logOut}>Log me out</button>
+          <button onClick={stayActive}>Keep me signed in</button>
+        </div>
+      </Modal>
+    </div>
+  )
+}
+
+export default App;
