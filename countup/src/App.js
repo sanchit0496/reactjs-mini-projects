ee30e89c262dc3844c14a390bd5c276da89468dc
Sanchit <sanchit0496@gmail.com>
2021-06-26 20:34:36 +0530

    adding countup

--- countup/src/App.js
+++ countup/src/App.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { render } from 'react-dom';
+import CountUp from 'react-countup';
+
+
 
 const App = () => {
     return (
         <div>
-            App
+      {/* <CountUp isCounting start={10} end={0} duration={10} easing="linear" /> */}
+      <CountUp isCounting end={2000} duration={3.2} />
+
         </div>
     )
 }
