0726a77384bb6b8520b367cca6538f98b1ffb332
Sanchit <sanchit0496@gmail.com>
2021-06-26 11:49:26 +0530

    the images coming on frontend api

--- image-search/src/App.js
+++ image-search/src/App.js
@@ -6,7 +6,7 @@ class App extends React.Component{
         super(props)
         this.state = {
             pics: [],
-            hasLoaded: false
+            
         }
         this.fetchPics = this.fetchPics.bind(this)
     }
@@ -15,24 +15,26 @@ class App extends React.Component{
     fetchPics(e){
         e.preventDefault();
         let a = document.getElementById('term').value;
-      
+        alert('clicked')
         let url = `https://pixabay.com/api/?key=22238304-8a144d939bdfe845e8130ae3b&q=${a}&image_type=photo`;
 
         fetch(url)
         .then(response => response.json())
-        .then(res => this.setState({pics: res, hasLoaded: true}))
+        .then(res => this.setState({pics: res.hits}))
     }
 
     render(){
         return(
             <div>
-                <form onSubmit>
+                <form action = '#' onSubmit = {this.fetchPics}>
                     <input type="text" id='term' placeholder = 'Enter Value'/>
-                    <input type="submit" onSubmit = {(e) => {this.fetchPics(e)}}/>
+                    <input type="submit" />
                 </form>
                 
                 {
-                    this.state.hasLoaded ? <span>Loaded</span> : <span>Loading</span>
+                    this.state.pics.map((photo) => {
+                        return <img src= {photo.largeImageURL} key={photo.id} />
+                    })
                 }
             </div>
         )
