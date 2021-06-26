52e597277bb9de415e93fe4e6031e1ab2eab4511
Sanchit <sanchit0496@gmail.com>
2021-06-26 15:12:40 +0530

    adding axios

--- react-pagination/src/App.js
+++ react-pagination/src/App.js
@@ -2,6 +2,18 @@ import React, {useState, useEffect} from 'react'
 
 const App = () => {
 
+    const [posts, setPosts] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [currentPage, setCurrentPage] = useState(1)
+    const[postsPerPage] = useState(10)
+
+    useEffect(() => {
+        effect
+        return () => {
+            cleanup
+        }
+    }, [input])
+
     return (
         <div>
             App
