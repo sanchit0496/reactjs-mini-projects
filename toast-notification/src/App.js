792f625002e244200df636346d14a778811aa9d5
Sanchit <sanchit0496@gmail.com>
2021-06-26 17:39:12 +0530

    updating

--- toast-notification/src/App.js
+++ toast-notification/src/App.js
@@ -4,18 +4,23 @@ import 'react-toastify/dist/ReactToastify.css';
 toast.configure()
 
 const notify = () => {
-    toast('Hello There 4', 
-    {position: toast.POSITION.BOTTOM_LEFT})
- toast('Hello There 6', 
-    {position: toast.POSITION.BOTTOM_RIGHT})
- toast('Hello There 5', 
-    {position: toast.POSITION.BOTTOM_CENTER})
- toast('Hello There 1', 
-    {position: toast.POSITION.TOP_LEFT})
- toast('Hello There 3', 
-    {position: toast.POSITION.TOP_RIGHT})
- toast('Hello There 2', 
-    {position: toast.POSITION.TOP_CENTER})}
+//     toast('Hello There 4', 
+//     {position: toast.POSITION.BOTTOM_LEFT})
+//  toast('Hello There 6', 
+//     {position: toast.POSITION.BOTTOM_RIGHT})
+//  toast('Hello There 5', 
+//     {position: toast.POSITION.BOTTOM_CENTER})
+//  toast('Hello There 1', 
+//     {position: toast.POSITION.TOP_LEFT})
+//  toast('Hello There 3', 
+//     {position: toast.POSITION.TOP_RIGHT})
+//  toast('Hello There 2', 
+//     {position: toast.POSITION.TOP_CENTER})
+
+    toast.success('successful')
+    // toast.info('info', {autoClose: 2000})
+
+}
 
 const App = () => {
     return (
